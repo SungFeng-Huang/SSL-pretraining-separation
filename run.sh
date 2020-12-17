86fd45121514683e1691aa28df4c2e290dcff489
b02901071 <r06942045@ntu.edu.tw>
2020-12-18 00:43:55 +0800

    add pre-training description

--- run.sh
+++ run.sh
@@ -108,7 +108,7 @@ if [[ -z ${tag} ]]; then
 	tag=${uuid}
 fi
 
-expdir=exp/train_${model}_${corpus}_${strategy}_${tag}
+expdir=exp/train_${model}_${corpus}_${task}_${strategy}_${tag}
 mkdir -p $expdir && echo $uuid >> $expdir/run_uuid.txt
 echo "Results from the following experiment will be stored in $expdir"
 
